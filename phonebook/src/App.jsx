7b3efa471d9b9212d54ca9fa1a3df8b52ea2d4d6
serfoll <sergiofollyvi@gmail.com>
2025-05-25 16:10:07 +0200

    add phone number

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -5,26 +5,35 @@ import { useState } from "react";
 const Persons = ({ persons }) => (
   <div>
     {persons.map((person) => (
-      <p key={person.name}>{person.name}</p>
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
     ))}
   </div>
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "12345678" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
   const [errorMsg, setErrMsg] = useState("");
 
   const addNewName = (e) => {
     e.preventDefault();
 
     if (!newName || newName === "") {
-      setErrMsg("Name can't be empty");
+      setErrMsg("Please provide a name!");
+      return;
+    } else if (!newNumber || newNumber === "") {
+      setErrMsg("Please provide a number!");
       return;
     }
 
     const newPerson = {
       name: newName.trim(),
+      number: newNumber.trim(),
     };
 
     if (persons.some((person) => person.name === newPerson.name)) {
@@ -47,13 +56,25 @@ const App = () => {
       )}
       <form onSubmit={addNewName}>
         <div>
-          <label htmlFor="newName">name:</label>
+          <label htmlFor="newName">name: </label>
           <input
             id="newName"
             name="newName"
-            placeholder="John Doe"
-            value={newName}
             onChange={(e) => setNewName(e.target.value)}
+            placeholder="John Doe"
+            required
+            value={newName}
+          />
+        </div>
+        <div>
+          <label htmlFor="newNumber">number: </label>
+          <input
+            id="newNumber"
+            name="newnewNumberName"
+            onChange={(e) => setNewNumber(e.target.value)}
+            placeholder="012-345 67 80"
+            required
+            value={newNumber}
           />
         </div>
         <div>
