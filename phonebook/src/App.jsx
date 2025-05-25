86df307e76daf66af2a511316a7bff720f6515b4
serfoll <sergiofollyvi@gmail.com>
2025-05-25 15:31:04 +0200

    Add name to phonebook

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,11 +1,61 @@
 /** @format */
 
-function App() {
+import { useState } from "react";
+
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map((person) => (
+      <p key={person.name}>{person.name}</p>
+    ))}
+  </div>
+);
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNewName = (e) => {
+    e.preventDefault();
+
+    if (!newName || newName === "") {
+      alert("Name can't be empty");
+      return;
+    }
+
+    const newPerson = {
+      name: newName,
+    };
+
+    console.log("add: ", newPerson);
+
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
   return (
     <div>
-      <h1>Phonebook App</h1>
+      <h1>Phonebook</h1>
+
+      <form onSubmit={addNewName}>
+        <div>
+          <label htmlFor="newName">name:</label>
+          <input
+            id="newName"
+            name="newName"
+            placeholder="John Doe"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
     </div>
   );
-}
+};
 
 export default App;
