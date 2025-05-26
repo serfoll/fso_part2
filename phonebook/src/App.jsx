9e5ea055937dec8f7d5d8b5bc88bf1500756fe7d
serfoll <sergiofollyvi@gmail.com>
2025-05-26 19:11:25 +0200

    get inital from json-server using axios and effect hook

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,17 +1,14 @@
 /** @format */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -49,6 +46,20 @@ const App = () => {
     setNewNumber("");
     setErrMsg("");
   };
+
+  useEffect(() => {
+    console.log("persons hook");
+    const serverUrl = "http://localhost:3001";
+    axios
+      .get(`${serverUrl}/persons`)
+      .then((res) => {
+        setPersons(res.data);
+      })
+      .catch((err) => {
+        console.error("request failed:", err);
+      });
+  }, []);
+  console.log("render", persons.length, "peronsons");
 
   return (
     <div>
