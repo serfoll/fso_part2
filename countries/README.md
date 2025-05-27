5d129dc230c19f6542aa0f5103c1ac44ca29755e
serfoll <sergiofollyvi@gmail.com>
2025-05-27 11:58:15 +0200

    ui basics

--- countries/README.md
+++ countries/README.md
@@ -1,3 +1,3 @@
 # Coutries
 
-A react app that displays information on different countries.
+A react app that displays information on different countries using https://studies.cs.helsinki.fi/restcountries REST API.
--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,9 +1,24 @@
 /** @format */
+import { useEffect, useState } from "react";
 
 function App() {
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    if (!filter || filter === "") return;
+    console.log("current filter: ", filter);
+  }, [filter]);
+
   return (
     <div>
-      <h1>Countries</h1>
+      <label htmlFor="countrie">find countries: </label>
+      <input
+        onChange={(e) => setFilter(e.target.value)}
+        id="countrie"
+        name="countrie"
+        type="text"
+        value={filter}
+      />
     </div>
   );
 }
